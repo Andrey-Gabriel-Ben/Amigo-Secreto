45265fa946ef0cdeced3f07f04f62860e5f5c9c0
andrey <andreygabrielben@gmail.com>
2026-01-29 20:53:55 -0300

    Adicionar uma verificação que invalida a função de adicionar se o nome adicionado for vazio.

--- js/app.js
+++ js/app.js
@@ -4,6 +4,11 @@ let listaAmigosHTML = document.getElementById("lista-amigos");
 function adicionar() {
     let nomeAmigo = document.getElementById("nome-amigo").value;
 
+    if (nomeAmigo == "") {
+        alert("Parece que você não adicionou o nome de nenhum amigo à lista, por favor, tente novamente.");
+        return;
+    };
+
     if (listaDeAmigos.includes(nomeAmigo)) {
         alert("Este nome já foi adicionado à lista de amigos a serem sorteados, caso esse seja outro amigo, favor adicionar mais algum nome para distingui-lo.");
         return;
